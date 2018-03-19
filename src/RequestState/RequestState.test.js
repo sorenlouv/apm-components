fedfd8584b5a9a361028485e6bf57ede34cbfc1d
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-03-19 16:39:26 +0100

    Updated test

--- src/RequestState/RequestState.test.js
+++ src/RequestState/RequestState.test.js
@@ -2,16 +2,22 @@ import React from 'react';
 import RequestState, { STATUSES } from './RequestState';
 import { mount } from 'enzyme';
 
+jest.useFakeTimers();
+
+function delay(ms, ...args) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`Resolved in ${ms}ms with [${args}]`);
+    }, ms);
+  });
+}
+
 describe('RequestState', () => {
   let wrapper, spy, promise;
 
   beforeEach(() => {
-    spy = jest.fn(id => {
-      promise = new Promise(resolve => {
-        setTimeout(() => {
-          resolve(`Resolved ${id}`);
-        }, 200);
-      });
+    spy = jest.fn((...args) => {
+      promise = delay(2000, ...args);
       return promise;
     });
 
@@ -45,36 +51,59 @@ describe('RequestState', () => {
   });
 
   it('should have data when promise fn resolves', async () => {
+    jest.runAllTimers();
     await promise;
 
     expect(spy).toHaveBeenCalledTimes(1);
     expect(wrapper.state()).toEqual({
-      data: 'Resolved myInitialId',
+      data: 'Resolved in 2000ms with [myInitialId]',
       error: null,
       status: STATUSES.SUCCESS
     });
     wrapper.update();
     expect(wrapper.html()).toEqual(
-      '<div>Status: SUCCESS; Data: Resolved myInitialId; Error: ;</div>'
+      '<div>Status: SUCCESS; Data: Resolved in 2000ms with [myInitialId]; Error: ;</div>'
     );
   });
 
   it('should update when args change', async () => {
+    jest.runAllTimers();
     await promise;
 
     wrapper.setProps({ args: ['mySecondId'] });
 
     expect(spy).toHaveBeenCalledTimes(2);
     expect(wrapper.state()).toEqual({
-      data: 'Resolved myInitialId',
+      data: 'Resolved in 2000ms with [myInitialId]',
       error: null,
       status: STATUSES.LOADING
     });
 
+    jest.runAllTimers();
     await promise;
 
     expect(wrapper.state()).toEqual({
-      data: 'Resolved mySecondId',
+      data: 'Resolved in 2000ms with [mySecondId]',
+      error: null,
+      status: STATUSES.SUCCESS
+    });
+  });
+
+  it('should render last changed prop - not last resolved prop ', async () => {
+    let shortPromise;
+    wrapper.setProps({
+      args: ['myThirdId'],
+      fn: (...args) => {
+        shortPromise = delay(100, args);
+        return shortPromise;
+      }
+    });
+
+    jest.runAllTimers();
+    await promise, shortPromise;
+
+    expect(wrapper.state()).toEqual({
+      data: 'Resolved in 100ms with [myThirdId]',
       error: null,
       status: STATUSES.SUCCESS
     });
